2c5af533599a6f7469a4576063106cffc94327dd
mundembe <61733352+mundembe@users.noreply.github.com>
2024-04-12 19:43:26 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Part1 {
+    internal class Program {
+        // global variables
+        public static int numOfIngredients, numOfSteps;
+        public static string line = "*******************************";
+        public static string[,] ingredients;
+        public static string[] steps;
+        public static double scale = 1.0;
+        public static bool exitMainLoop = false;
+
+        
+
+    }
+}
